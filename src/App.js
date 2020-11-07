4dcbca9559831a8dfaa72f120125e725a4c6a4a3
NikosNt <TreNikosD13@gmail.com>
2020-11-07 19:10:03 +0200

    Made main directories

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Thesis Begin</h1>
-      </header>
-    </div>
-  );
+const app = () => {
+
+  return(
+        <div className="App">
+        <header className="App-header">
+          <h1>Thesis Begin</h1>
+        </header>
+      </div>
+  )
+
 }
 
-export default App;
+export default app;
