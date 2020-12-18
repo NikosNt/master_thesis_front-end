2f0da34e72548ddee5fa93fd45c8a90a3e2f0c43
NikosNt <TreNikosD13@gmail.com>
2020-12-18 12:10:58 +0200

    test commit ftom laptop

--- src/store/actions/UserAction.js
+++ src/store/actions/UserAction.js
@@ -8,118 +8,119 @@ export const loadFail = (error) => {
     };
 };
 
-export const setSearchText =(text)=>{
-    return{
-      type:actionTypes.SEARCH_TEXT  ,
-      searchText:text
-    }   
+export const setSearchText = (text) => {
+    return {
+        type: actionTypes.SEARCH_TEXT,
+        searchText: text
+    }
 }
 
-export const citiesInit =(cities)=>{
-    return{
-        type:actionTypes.CITIES_INIT,
-        cities:cities 
+export const citiesInit = (cities) => {
+    return {
+        type: actionTypes.CITIES_INIT,
+        cities: cities
     }
 }
 export const fetchCities = () => {
     return dispatch => {
         axios.get('api/services/business/by/cities')
-        .then( res => {
-            let citiess = [];
-            citiess = [
-                {name:"All cities" ,id:0},
-            ]
-            for(let key in res.data){
-                citiess.push({
-                    name:res.data[key],
-                    id:parseInt(res.data[key].id)+1,
-                });
-            }
-        dispatch(citiesInit(citiess));
-        })
-        .catch(err => {
-            console.log(err)
-            dispatch(loadFail(err))
-        });
+            .then(res => {
+                let citiess = [];
+                citiess = [
+                    { name: "All cities", id: 0 },
+                ]
+                for (let key in res.data) {
+                    citiess.push({
+                        name: res.data[key],
+                        id: parseInt(res.data[key].id) + 1,
+                    });
+                }
+                dispatch(citiesInit(citiess));
+            })
+            .catch(err => {
+                console.log(err)
+                dispatch(loadFail(err))
+            });
     }
 
 }
 
-export const servicesInit =(services)=>{
-    return{
-        type:actionTypes.SERVICES_INIT,
-        services:services 
+export const servicesInit = (services) => {
+    return {
+        type: actionTypes.SERVICES_INIT,
+        services: services
     }
 }
 
 export const fetchServices = () => {
     return dispatch => {
         axios.get('api/services/business/by/types')
-        .then( res => {
-            let types = [];
-            types = [
-                {name:"All types" ,id:0},
-               // {name:"none" ,id:1},
-            ]
-            for(let key in res.data){
-                types.push({
-                    name:res.data[key],
-                    id:parseInt(res.data[key].id)+1,
-                });
-            }
-        dispatch(servicesInit(types));
-        })
-        .catch(err => {
-            dispatch(loadFail(err))
-        });
+            .then(res => {
+                let types = [];
+                types = [
+                    { name: "All types", id: 0 },
+                    // {name:"none" ,id:1},
+                ]
+                for (let key in res.data) {
+                    types.push({
+                        name: res.data[key],
+                        id: parseInt(res.data[key].id) + 1,
+                    });
+                }
+                dispatch(servicesInit(types));
+            })
+            .catch(err => {
+                dispatch(loadFail(err))
+            });
     }
 
 }
 
-export const updateCityContent =(content)=>{
-    return{
-      type:actionTypes.CITY_CONTENT  ,
-      cityContent:content
-    }   
+export const updateCityContent = (content) => {
+    return {
+        type: actionTypes.CITY_CONTENT,
+        cityContent: content
+    }
 }
 
-export const updateServiceContent =(content)=>{
-    return{
-      type:actionTypes.SERVICE_CONTENT  ,
-      serviceContent:content
-    }   
+export const updateServiceContent = (content) => {
+    return {
+        type: actionTypes.SERVICE_CONTENT,
+        serviceContent: content
+    }
 }
 
 
-export const loadServicesCompanies =(content)=>{
-    return{
-      type:actionTypes.LOAD_SERVICES_COMPANIES  ,
-      loadedServices_Companies:content
-    }   
+export const loadServicesCompanies = (content) => {
+    return {
+        type: actionTypes.LOAD_SERVICES_COMPANIES,
+        loadedServices_Companies: content
+    }
 }
 
-export const fetchServicesCompanies=(city,typeBusiness,searchText)=>{
-    if(!city){ city = "empty" }
-    if(!typeBusiness){ typeBusiness = "empty"}
-    if(searchText === ""){ searchText="empty"}
+export const fetchServicesCompanies = (city, typeBusiness, searchText) => {
+    if (!city) { city = "empty" }
+    if (!typeBusiness) { typeBusiness = "empty" }
+    if (searchText === "") { searchText = "empty" }
 
     return dispatch => {
-        axios.get('api/services/business/by/'+city+'/'+typeBusiness+'/'+searchText+'/')
-        .then( res => {
-            const all_business = [];        
-            for(let key in res.data){
-                all_business.push({
-                    ...res.data[key]
-                });  
-            }     
-            dispatch(loadServicesCompanies(all_business));
-        })
-        .catch(err => {
-            dispatch(loadFail(err))
-        });
+        axios.get('api/services/business/by/' + city + '/' + typeBusiness + '/' + searchText + '/')
+            .then(res => {
+                const all_business = [];
+                for (let key in res.data) {
+                    all_business.push({
+                        ...res.data[key]
+                    });
+                }
+                dispatch(loadServicesCompanies(all_business));
+            })
+            .catch(err => {
+                dispatch(loadFail(err))
+            });
     }
 }
 
+//test
 
 // export const fetchServicesCompanies=(city,typeBusiness,searchText)=>{
 //     console.log("city ->",city," typeBusiness ->",typeBusiness," searchText ->",searchText)
